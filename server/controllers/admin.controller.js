ed11bbded84a7817f47c5ae4cff13b94b8842847
Debashis kar <debashis99374@gmail.com>
2023-12-03 14:49:02 +0530

    offers part finished

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -2,6 +2,7 @@
 
 //in this page we are doing admin control pannel
 
+const OfferForFoodDelevery = require("../models/offers.model")
 const UserForFoodDeliveryApp = require("../models/user.model")
 //toggle admin status
 const toggleAdmin=async(req,res)=>{
@@ -36,7 +37,7 @@ const toggleRestaurantOwner=async (req,res)=>{
     const ownerValue=req.body.restaurantOwner
     try{
         const loggedInUser=await UserForFoodDeliveryApp.findById(userId)
-        const userToBeToggled=await UserForFoodDeliveryApp.findById(userId2)
+        
         if(!loggedInUser){
             return res.status(400).json({message:"you are not logged in"})
          }
@@ -51,9 +52,98 @@ const toggleRestaurantOwner=async (req,res)=>{
         res.status(500).json({message:`error is : ${err}`})
     }
 }
+//delete any user
+const deleteUser=async(req,res)=>{
+    const userId=req.user._id
+    const userId2=req.body._id
+    try{
+        const loggedInUser=await UserForFoodDeliveryApp.findById(userId)
+        const userToBeDeleted=await UserForFoodDeliveryApp.findById(userId2)
+        if(!loggedInUser){
+            return res.status(400).json({message:"you are not logged in"})
+         }
+         if(loggedInUser.isAdmin===false){
+             return res.status(401).json({message:"Unauthorised:only admins can delete any account"})
+         }
+         if(loggedInUser._id.equals(userToBeDeleted._id)){
+            return res.status(402).json({message:"Unauthorised:you cant delete your own account"})
+        }
+         const deletedUser=await UserForFoodDeliveryApp.findByIdAndDelete(userId2)
+         return res.status(200).json({message:`${deletedUser.firstName}'s account deleted`,data:deletedUser})
+
+    }catch(err){
+        
+        res.status(500).json({message:`error is : ${err}`}) 
+    }
+}
+//coupone code related controllers
+//all coupons/offers
+const allCoupons=async(req,res)=>{
+    try{
+        const allCoupons=await OfferForFoodDelevery.find({})
+        if(!allCoupons){
+            return res.status(400).json({message:"cant fetch all coupons"})
+        }
+        return res.status(200).json({message:`All coupons are:`,data:allCoupons})
+
+    }catch(err){
+        
+        res.status(500).json({message:`error is : ${err}`}) 
+    }
+}
+//create a coupon
+const createCoupon=async(req,res)=>{
+    const userId=req.user._id
+    const {name,discountAmount}=req.body
+    try{
+        const loggedInUser=await UserForFoodDeliveryApp.findById(userId)
+        if(!loggedInUser){
+            return res.status(400).json({message:"you are not logged in"})
+         }
+         if(loggedInUser.isAdmin===false){
+             return res.status(401).json({message:"Unauthorised:only admins can create a coupon"})
+         }
+         const newCoupon=await OfferForFoodDelevery.create({name,discountAmount})
+         return res.status(200).json({message:`New coupone code added`,data:newCoupon})
+
+    }catch(err){
+        
+        res.status(500).json({message:`error is : ${err}`}) 
+    }
+}
+//delete a coupon
+
+const deleteCoupon=async(req,res)=>{
+    const userId=req.user._id
+    const couponId=req.body._id
+    try{
+        const loggedInUser=await UserForFoodDeliveryApp.findById(userId)
+        const foundCoupon=await OfferForFoodDelevery.findById(couponId)
+        if(!loggedInUser){
+            return res.status(400).json({message:"you are not logged in"})
+         }
+         if(loggedInUser.isAdmin===false){
+             return res.status(401).json({message:"Unauthorised:only admins can delete a coupon"})
+         }
+         if(!foundCoupon){
+            return res.status(400).json({message:"coupon not found"})
+         }
+         const deletedCoupon=await OfferForFoodDelevery.findByIdAndDelete(couponId)
+
+return res.status(200).json({message:`Coupon deleted`,data:deletedCoupon})
+    }catch(err){
+        
+        res.status(500).json({message:`error is : ${err}`}) 
+    }
+}
 
 
 module.exports={
     toggleAdmin,
-    toggleRestaurantOwner
+    toggleRestaurantOwner,
+    deleteUser,
+    //offer related
+    allCoupons,
+    createCoupon,
+    deleteCoupon
 }
--- server/routes/admin-routes.js
+++ server/routes/admin-routes.js
@@ -2,9 +2,16 @@
 
 const express=require('express')
 const adminRouter=express.Router()
-const {toggleAdmin,toggleRestaurantOwner}=require('../controllers/admin.controller')
+const {toggleAdmin,toggleRestaurantOwner,deleteUser,allCoupons,createCoupon,deleteCoupon}=require('../controllers/admin.controller')
 const authVarify = require('../middlewheres/auth.varify')
+//admins toggle related routes
 adminRouter.post('/admin-toggleAdmin',authVarify,toggleAdmin)
 adminRouter.post('/admin-toggleRestaurant-owner',authVarify,toggleRestaurantOwner)
+adminRouter.post('/admin-delete-user',authVarify,deleteUser)
+
+//admins coupon/offer related routes
+adminRouter.get('/admin-all-offers',allCoupons)
+adminRouter.post('/admin-create-a-offer',authVarify,createCoupon)
+adminRouter.post('/admin-delete-a-offer',authVarify,deleteCoupon)
 
 module.exports=adminRouter
